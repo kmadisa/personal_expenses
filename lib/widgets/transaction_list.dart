628031ee1e620cce4dd083676b007fd976114edf
kmadisa <katleho.madisa47@gmail.com>
2020-08-31 20:14:09 +0000

    Calculating Sizes Dynamically

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,61 +11,58 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
+    return transactions.isEmpty
+      ? Column(
+          children: <Widget>[
+            Text(
+              'No transactions added yet!',
+              style: Theme.of(context).textTheme.title,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        )
+      : ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 5,
+              ),
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: FittedBox(
+                        child: Text('\$${transactions[index].amount}')),
+                  ),
+                ),
+                title: Text(
+                  transactions[index].title,
                   style: Theme.of(context).textTheme.title,
                 ),
-                SizedBox(
-                  height: 30,
+                subtitle: Text(
+                  DateFormat.yMMMd().format(transactions[index].date),
                 ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => deleteTransaction(index),
+                  color: Theme.of(context).errorColor,
                 ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text('\$${transactions[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => deleteTransaction(index),
-                      color: Theme.of(context).errorColor,
-                    ),
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        );
   }
 }
