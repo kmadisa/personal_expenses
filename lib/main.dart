6e8b8a29584d5d09519a3694271fc6e7a3a7e05c
kmadisa <katleho.madisa47@gmail.com>
2020-08-31 20:44:11 +0000

    Using the LayoutBuilder widget

--- lib/main.dart
+++ lib/main.dart
@@ -110,13 +110,13 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               height: (MediaQuery.of(context).size.height -
                        appBar.preferredSize.height -
-                       MediaQuery.of(context).padding.top) * 0.4,
+                       MediaQuery.of(context).padding.top) * 0.25,
               child: Chart(_recentTransactions),
             ),
             Container(
               height: (MediaQuery.of(context).size.height -
                        appBar.preferredSize.height -
-                       MediaQuery.of(context).padding.top) * 0.6,
+                       MediaQuery.of(context).padding.top) * 0.75,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
           ],
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,19 +9,20 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(builder: (ctx, constraints){
+      return Column(
       children: <Widget>[
         Container(
-          height: 20,
+          height: constraints.maxHeight * 0.15,
           child: FittedBox(
             child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
           ),
         ),
         SizedBox(
-          height: 4,
+          height: constraints.maxHeight * 0.05,
         ),
         Container(
-          height: 60,
+          height: constraints.maxHeight * 0.6,
           width: 10,
           child: Stack(
             children: <Widget>[
@@ -47,10 +48,16 @@ class ChartBar extends StatelessWidget {
           ),
         ),
         SizedBox(
-          height: 4,
+          height: constraints.maxHeight * 0.05,
         ),
-        Text(label),
+        Container(
+          height: constraints.maxHeight * 0.15,
+          child: FittedBox(
+            child: Text(label),
+          ),
+        ),
       ],
     );
+    },); 
   }
 }
