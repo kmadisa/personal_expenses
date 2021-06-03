940e4e706b4b4dc82e95397cd3cf0892b1e0512b
Katleho Madisa <katleho.madisa47@gmail.com>
2021-06-03 22:49:23 +0000

    155 Using Keys

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,13 +32,12 @@ class TransactionList extends StatelessWidget {
               ],
             );
           })
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return TransactionItem(
-                  transaction: transactions[index],
-                  deleteTransaction: deleteTransaction);
-            },
-            itemCount: transactions.length,
-          );
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                    key: ValueKey(tx.id),
+                    transaction: tx,
+                    deleteTransaction: deleteTransaction))
+                .toList());
   }
 }
